cc8c02e6219459a36a1ea99d3dd8063678f7e66f
Olga <olga.myazina@outlook.com>
2018-10-07 09:14:45 +0300

    завершение работы

--- public/js/index.js
+++ public/js/index.js
@@ -48,16 +48,20 @@ function createEventElement(event) {
     if (data.type === 'graph') {
       //вставляем картинку-заглушку
       const graph = document.querySelector('.template-graph').content.cloneNode(true);
+      graph.querySelector('.graph').style.background = ` url('../img/Richdata.png') no-repeat`;
       graph.querySelector(
         '.graph'
       ).style.backgroundImage = `-webkit-image-set( url('../img/Richdata.png') 1x, url('img/Richdata@2x.png') 2x, url('img/Richdata@3x.png') 3x )`;
+      graph.querySelector('.graph').style.backgroundPosition = `center bottom`;
       tile.querySelector('.data-tile').appendChild(graph);
     }
     //Нашли изображение
     if (data.image) {
       //вставляем заглушку вместо "get_it_from_mocks_:3.jpg"
       const imgDiv = document.createElement('div');
+      imgDiv.style.background = `url('img/Bitmap.png')`;
       imgDiv.style.background = `-webkit-image-set( url('img/Bitmap.png') 1x, url('img/Bitmap2x.png') 2x , url('img/Bitmap3x.png') 3x`;
+      imgDiv.style.backgroundPosition = `50% 50%`;
       imgDiv.classList.add('cam');
       const image = document.querySelector('.template-image').content.cloneNode(true);
       image.appendChild(imgDiv);
@@ -69,6 +73,9 @@ function createEventElement(event) {
       controlBright.classList.add('control-bright');
       controlZoom.innerText = `Приближение: 78%`;
       controlBright.innerText = `Яркость: 50%`;
+      const camSlider = document.createElement('div');
+      camSlider.classList.add(`cam-slider`);
+      imgDiv.appendChild(camSlider);
       labels.appendChild(controlZoom);
       labels.appendChild(controlBright);
       tile.querySelector('.data-tile').classList.add('camera');
@@ -96,56 +103,47 @@ function pageLoad() {
   //Получаем элемент - камеры - картинки, чтобы вращать
   const cam = document.querySelector('.camera');
   cam.setAttribute('touch-action', 'none');
+  const el = document.querySelector('.cam');
+  el.setAttribute('touch-action', 'none');
+  const zoom = document.querySelector('.control-zoom');
+  const bright = document.querySelector('.control-bright');
+  const camSlider = document.querySelector('.cam-slider');
+  camSlider.style.opacity = '1';
 
   /* */
   const evCache = [];
   let prevDiff = -1;
-  let prevDist = -1;
   //массив предыдущих координат
   const prevCoord = [];
   const startCoord = [];
   let currentScale = 100,
-    currentPos = 50;
+    currentPos = 100;
   init();
 
   function init() {
-    // Install event handlers for the pointer target
-    const el = document.querySelector('.cam');
+    //навешиваем слушателей на события
+
     el.addEventListener('pointerdown', pointerdown_handler);
     el.addEventListener('pointermove', pointermove_handler);
-
-    // Use same handler for pointer{up,cancel,out,leave} events since
-    // the semantics for these events - in this app - are the same.
     el.addEventListener('pointerup', pointerup_handler);
+
     el.addEventListener('pointercancel', pointerup_handler);
     el.addEventListener('pointerout', pointerup_handler);
     el.addEventListener('pointerleave', pointerup_handler);
+    camSlider.style.left = `${currentPos * 0.75 + 15}px`;
   }
 
   function pointerdown_handler(ev) {
-    // The pointerdown event signals the start of a touch interaction.
-    // This event is cached to support 2-finger gestures
     evCache.push(ev);
     startCoord.push({
       x: ev.clientX,
       y: ev.clientY
     });
-    console.log('pointerDown', ev);
   }
 
   function pointermove_handler(ev) {
-    // This function implements a 2-pointer horizontal pinch/zoom gesture.
-    //
-    // If the distance between the two pointers has increased (zoom in),
-    // the taget element's background is changed to "pink" and if the
-    // distance is decreasing (zoom out), the color is changed to "lightblue".
-    //
-    // This function sets the target element's border to "dashed" to visually
-    // indicate the pointer's target received a move event.
-    console.log('pointerMove', ev);
-
     // Find this event in the cache and update its record with this event
-    for (var i = 0; i < evCache.length; i++) {
+    for (let i = 0; i < evCache.length; i++) {
       if (ev.pointerId == evCache[i].pointerId) {
         evCache[i] = ev;
         break;
@@ -154,35 +152,26 @@ function pageLoad() {
 
     // If two pointers are down, check for pinch gestures
     if (evCache.length === 2) {
-      // Calculate the distance between the two pointers
-      var curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+      // Расстояние между пальцами по х
+      let curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
       let distX = evCache[0].clientX - evCache[1].clientX;
       let distY = evCache[0].clientY - evCache[1].clientY;
-      let currDist = Math.sqrt(Math.pow(distX, 2) + Math.pow(distY, 2));
-      let distXprev, distXstart, distYstart;
-      let distYprev;
-      let t;
-      let prevDist;
-      let a, newA, lastA;
+      let distXstart, distYstart;
+
+      let a, gradA;
       if (startCoord.length > 0) {
         distXstart = startCoord[0].x - startCoord[1].x;
         distYstart = startCoord[0].y - startCoord[1].y;
-        /*
-        t = distXstart * distX + distYstart * distY;
-        prevDist = Math.sqrt(Math.pow(distXstart, 2) + Math.pow(distYstart, 2));
-        a = Math.acos(t / (currDist * prevDist));
-        newA = (a * 180) / Math.PI;
-      */
 
         a = Math.atan((distXstart * distY - distYstart * distX) / (distXstart * distX + distYstart * distY));
-        newA = (a * 180) / Math.PI;
+        gradA = (a * 180) / Math.PI;
       }
 
-      if (Math.abs(newA) > 5) {
-        console.log(`100+угол: ${100 + newA}`);
+      if (Math.abs(gradA) > 5) {
         //значит поворот
-        cam.style.webkitFilter = `brightness(${100 + newA}%)`;
-        cam.style.filter = `brightness(${100 + newA}%)`;
+        el.style.webkitFilter = `brightness(${100 + gradA}%)`;
+        el.style.filter = `brightness(${100 + gradA}%)`;
+        bright.innerText = `Яркость ${Math.round(100 + gradA)}%`;
       }
       {
         console.log(`currentScale: ${currentScale}, curDiff: ${curDiff}, prevDiff: ${prevDiff} `);
@@ -197,10 +186,12 @@ function pageLoad() {
         if (prevDiff > 0) currentScale += curDiff - prevDiff;
         if (currentScale > 200) currentScale = 200;
         if (currentScale < 50) currentScale = 50;
-        cam.style.backgroundSize = `${currentScale}% ${currentScale}%`;
+        el.style.backgroundSize = `${currentScale}% ${currentScale}% `;
+        el.style.webkitBackgroundSize = `${currentScale}% ${currentScale}% `;
+        zoom.innerText = `Приближение ${currentScale}%`;
       }
 
-      // Cache the distance for the next move event
+      // сохраяняем начальные и предыдущие координаты
       prevDiff = curDiff;
       prevCoord.push({
         x: evCache[0].clientX,
@@ -210,79 +201,42 @@ function pageLoad() {
         x: evCache[1].clientX,
         y: evCache[1].clientY
       });
-      lastA = newA;
     }
 
     if (evCache.length === 1) {
       // значит перемещение влево или вправо (swipe)
-      cam.style.backgroundPositionX = `${currentPos + startCoord[0].x - ev.clientX}%`;
-      console.log(`свайп от ${currentPos + startCoord[0].x} to ${currentPos + startCoord[0].x - ev.clientX}`);
+      el.style.backgroundPositionX = `${currentPos}%`;
+      el.style.webkitBackgroudPositionSize = `${currentPos}%`;
+
+      if (startCoord.length > 0) {
+        currentPos += startCoord[0].x - ev.clientX;
+      }
+      if (currentPos < 0) currentPos = 0;
+      if (currentPos > 200) currentPos = 200;
+      camSlider.style.left = `${currentPos * 0.75 + 15}px`;
     }
   }
 
   function pointerup_handler(ev) {
     console.log(ev.type, ev);
-    // Remove this pointer from the cache and reset the target's
-    // background and border
-    remove_event(ev);
+    // Удаление предыдущих и начальных координат
+    remove_event(ev, evCache);
 
-    // If the number of pointers down is less than two then reset diff tracker
     if (evCache.length < 2) {
       prevDiff = -1;
       //Удаляем стартовые координаты
-      for (var i = 0; i < prevCoord.length; i++) {
-        prevCoord.splice(i, 1);
-        break;
-      }
-      for (var i = 0; i < startCoord.length; i++) {
-        startCoord.splice(i, 1);
-        break;
-      }
+      remove_event(ev, prevCoord);
+      remove_event(ev, startCoord);
     }
   }
 
-  function remove_event(ev) {
+  function remove_event(ev, arr) {
     // Remove this event from the target's cache
-    for (var i = 0; i < evCache.length; i++) {
-      if (evCache[i].pointerId == ev.pointerId) {
-        evCache.splice(i, 1);
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].pointerId == ev.pointerId) {
+        arr.splice(i, 1);
         break;
       }
     }
   }
-
-  // Log events flag
-  var logEvents = false;
-
-  // Logging/debugging functions
-  function enableLog(ev) {
-    logEvents = logEvents ? false : true;
-  }
-
-  /*Зум: получаем первый pointer.id и ждём второй. 
-Когда есть два - считаем между ними расстояние => 
-увеличение - прилижение, уменьшение- отдаление */
-
-  /*Использование pointer lock */
-
-  //запрашиваем pointer lock для элемента
-  //cam.requestPointerLock = cam.requestPointerLock || cam.webkitRequestPointerLock || cam.mozRequestPointerLock;
-
-  //выводим из режима захвата курсора
-  //document.exitPointerLock();
-
-  //для какого элемента включен pointer lock
-  //cam = document.pointerLockElement;
-
-  //событие для включения/выключения режима pointer lock
-  /*
-  document.addEventListener('pointerlockchange', e => {
-    //обрабатываем
-    console.log(`pointerlockchange: ${e}`);
-  });
-  //обработка ошибок
-  document.addEventListener('pointerlockerror', e => {
-    console.error(`Случилась ошибка при работе pointer lock: ${e}`);
-  });
-  */
 }
