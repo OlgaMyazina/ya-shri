9773afe80dfe79284648a07d546bbd4cd76d4ab0
Olga <olga.myazina@outlook.com>
2018-10-11 16:15:39 +0300

    Сделана анимация

--- public/js/device.js
+++ public/js/device.js
@@ -1,6 +1,5 @@
 function initVideo(video, url) {
   if (Hls.isSupported()) {
-    console.log('support hls');
     var hls = new Hls();
     hls.loadSource(url);
     hls.attachMedia(video);
@@ -8,7 +7,6 @@ function initVideo(video, url) {
       video.play();
     });
   } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    console.log(`not support`);
     video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
     video.addEventListener('loadedmetadata', function() {
       video.play();
@@ -16,7 +14,6 @@ function initVideo(video, url) {
   }
 }
 
-console.log('here');
 initVideo(
   document.getElementById('video-1'),
   'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8'
