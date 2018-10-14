78b5afce3f2424506d70bbbff352a8fa538b32c0
Olga <olga.myazina@outlook.com>
2018-10-14 14:49:52 +0300

    Подключение nodemon

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get('/', (request, response) => {
-  response.send('Hello from express');
+  response.send('test mon');
 });
 
 app.listen(port, err => {
