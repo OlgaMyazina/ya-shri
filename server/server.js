ead00bff0953f4c7db616969837a9ed0dc117e4a
Olga <olga.myazina@outlook.com>
2018-10-14 18:44:41 +0300

    Добавлена фильтрация по типу

--- server/server.js
+++ server/server.js
@@ -13,9 +13,10 @@
 Перейти на POST-параметры.
 Сделать пагинацию событий — придумать и реализовать API,
 позволяющее выводить события постранично.
-Д
 */
+
 const data = require('./data/events');
+const dataTypes = require('./data/config');
 
 const express = require('express');
 const app = express();
@@ -27,27 +28,44 @@ app.use((request, response, next) => {
   next();
 });
 
-const requestTime = (request, response, next) => {
-  //request.requestTime = time_format(Date.now());
-  //console.log(time_format(Date.now()));
-  next();
-};
-
-app.use(requestTime);
-
 app.get('/status', (request, response) => {
-  const now = Date.now();
-  response.send(
-    `старт сервера: ${timeFormat(timeServerStart)}, текущее время: ${timeFormat(now)}, разница: ${timeDiff(
-      timeServerStart,
-      now
-    )}`
-  );
+  response.send(`${timeDiff(timeServerStart, Date.now())}`);
 });
 
 app.get('/api/events', (request, response) => {
   let { events } = data;
-  response.send({ events });
+  //Если есть параметры
+  if (request.query) {
+    //Если это type
+    if (request.query.type) {
+      const { types } = dataTypes;
+      const queryTypes = request.query.type.split(':');
+      //проверяем типы из запроса queryTypes на наличие в конф. types
+      const unCorrectTypes = queryTypes.find(type => !types.includes(type));
+      console.log(`correctTypes ${unCorrectTypes}`);
+      if (unCorrectTypes) {
+        response.status(400).send(`incorrect type`);
+      } else {
+        const eventsNew = events.filter(event => event.type === queryTypes[0]);
+        response.send(queryTypes.map(type => events.filter(event => event.type === type)));
+      }
+    }
+  }
+  app.use((err, request, response, next) => {
+    // логирование ошибки, пока просто console.log
+    console.log(err);
+    response.status(500).send(`Something broke!`);
+    next();
+  });
+  /*
+  console.log(request.params);
+  console.log(request.query);
+  console.log(request.body);
+  */
+  response.json({
+    events
+  });
+  console.log(request.params);
 });
 
 app.use(function(req, res) {
@@ -59,10 +77,10 @@ app.listen(port, err => {
     return console.log('something bad', err);
   }
   timeServerStart = Date.now();
-
   console.log(`server listening on ${port}`);
 });
 
+/*
 function timeFormat(date) {
   options = {
     hour: 'numeric',
@@ -72,6 +90,7 @@ function timeFormat(date) {
   };
   return new Intl.DateTimeFormat('ru-RU', options).format(date);
 }
+*/
 
 function timeDiff(timeStart, timeCurrent) {
   const tDiff = new Date(timeCurrent - timeStart);
@@ -82,21 +101,3 @@ function timeDiff(timeStart, timeCurrent) {
     `0${tDiff.getUTCSeconds()}`.slice(-2)
   ].join(':');
 }
-
-/*
-app.use((request, response, next) => {
- console.log(request.headers);
- next();
-})
-app.use((request, response, next) => {
- request.chance = Math.random();
- next();
-})
-app.get('/', (request, response) => {
- response.json({
- chance: request.chance
- })
-})
-app.get("/api/events", eventsHandler);
-
-*/
