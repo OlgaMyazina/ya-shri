f9f0670ffd0dcc2f5893357f11e8735eea60f8f9
Olga <olga.myazina@outlook.com>
2018-10-17 15:44:02 +0300

    По замечанию: https://github.com/OlgaMyazina/ya-shri/pull/2#discussion_r225403778 Убрала типы из файла config.json, теперь типы собираются во множество из events.json
    По замечанию: https://github.com/OlgaMyazina/ya-shri/pull/2#discussion_r225403924
    Удаляем куски кода, которые находятся в комментарии

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const data = require('./data/events');
-const config = require('./data/config');
 
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 8000;
+const port = 8001;
 let timeServerStart;
+let pageLimit = 10;
 
 app.use(cors());
 
@@ -18,18 +18,20 @@ app.get('/api/events', (request, response) => {
   let { events } = data;
   //Если type есть в параметрах запроса
   if (request.query.type) {
-    const expectedTypes = config.types;
+    //множество типов. Смотрим тип у каждого события и добавляем в множество
+    const expectedTypes = new Set();
+    events.forEach(event => expectedTypes.add(event.type));
     const queryTypes = request.query.type.split(':');
-    //проверяем типы из запроса queryTypes на наличие в конф. types
-    const hasIncorrectType = queryTypes.some(type => !expectedTypes.includes(type));
+    //проверяем типы из запроса queryTypes на наличие во множестве типов
+    const hasIncorrectType = queryTypes.some(type => !expectedTypes.has(type));
     hasIncorrectType
       ? response.status(400).send(`incorrect type`)
       : (events = events.filter(event => queryTypes.includes(event.type)));
   }
-  //Если не указан limit, то будет 10
+  //Если не указан limit, то будет pageLimit= 10
   //Если не указана page, то будет 1
   if (request.query.page) {
-    const limit = request.query.limit ? parseInt(request.query.limit) : config.pageLimit;
+    const limit = request.query.limit ? parseInt(request.query.limit) : pageLimit;
     const page = parseInt(request.query.page);
     events = events.slice(limit * (page - 1), limit * page);
   }
@@ -56,24 +58,3 @@ function timeDiff(timeStart, timeCurrent) {
     `0${tDiff.getUTCSeconds()}`.slice(-2)
   ].join(':');
 }
-
-/*
-app.use((err, request, response, next) => {
-  // логирование ошибки, пока просто console.log
-  console.log(err);
-  response.status(500).send(`Something broke!`);
-  next();
-});
-*/
-
-/*
-function timeFormat(date) {
-  options = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false
-  };
-  return new Intl.DateTimeFormat('ru-RU', options).format(date);
-}
-*/
